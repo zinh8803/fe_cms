c9e36e4e2bcbc203419d25df48fc3231f09f9a19
zinh8803 <ngoquocvinh2003@gmail.com>
2026-05-25 23:26:19 +0700

    deploy: add env

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
